2e3a15ea881396a35eab1e6754d28e3518fbc8cb
Grandez <javier.gonzalez.grandez@gmail.com>
2022-02-21 17:37:16 +0100

    Setup

--- YATASetup/yata_linux.sh
+++ YATASetup/yata_linux.sh
@@ -3,9 +3,17 @@
 # uso: yata_linux.sh [root]
 
 # Check git
-git2 > /dev/null 2> /dev/null
+git > /dev/null 2> /dev/null
 if [ $? -eq 127 ] ; then
    echo "git must be installed"
    exit 127
 fi
-   
+
+echo "Getting YATA"   
+git clone https://github.com/Grandez/YATA2 $1
+rc=$?
+if [ $rc -ne 0 ] ; then
+   echo "Error " $rc " getting YATA"
+   exit 127
+fi   
+      
