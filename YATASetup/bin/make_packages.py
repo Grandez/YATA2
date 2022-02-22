489288c74c82856fbe402e43bbbf4b845445e053
Grandez <javier.gonzalez.grandez@gmail.com>
2022-02-22 12:50:41 +0100

    Setup

--- YATASetup/bin/make_packages.py
+++ YATASetup/bin/make_packages.py
@@ -3,6 +3,10 @@
 import sys
 import os, pwd, subprocess
 import configparser
+import shutil
+
+sys.path.insert(0, '../python')
+import yatatools as tools
 
 def demote(user_uid, user_gid):
     def result():
@@ -31,18 +35,18 @@ def exec_cmd(username):
     proc.wait()
 
 def make_package(package):
-    print("Making package " + package)
-    oldwd = os.getcwd()
-    os.chdir("/home/yata/YATA2/" + package)
+    tools.msg("Making package " + package)
+    #oldwd = os.getcwd()
+    #os.chdir("/home/yata/YATA2/" + package)
 
-    pw_record = pwd.getpwnam('yata')
+    pw_record = pwd.getpwnam(os.getlogin())
     homedir = pw_record.pw_dir
     user_uid = pw_record.pw_uid
     user_gid = pw_record.pw_gid
     env = os.environ.copy()
     env.update({'HOME': homedir, 'LOGNAME': 'yata', 'PWD': os.getcwd()})
 
-    pkg='/home/yata/YATA2/' + package
+    pkg = os.environ('YATA_ROOT') + package
     #proc = subprocess.Popen(['R', 'CMD', 'INSTALL', '--no-multiarch', '--with-keep.source', pkg],
     proc = subprocess.Popen(['R CMD INSTALL --no-multiarch --with-keep.source ' + pkg],
                               shell=True,
@@ -51,10 +55,15 @@ def make_package(package):
                               stdout=subprocess.PIPE
                               )
     proc.wait()
-    os.chdir(oldwd)
+    #os.chdir(oldwd)
     return proc.returncode
 
-file_ini = "YATA.cfg"
+oldwd = os.getcwd()
+yataroot = os.environ('YATA_ROOT')
+if not yataroot :
+    tools.fatal(16, "Missing environment variable YATA_ROOT")
+
+file_ini = yataroot + '/YATASetup/yata.cfg'
 n = len(sys.argv)
 if (n > 1) :
     file_ini = sys.argv[0]
@@ -62,10 +71,21 @@ if (n > 1) :
 config = configparser.ConfigParser()
 config.read(file_ini)
 
+os.chdir(yataroot)
+tools.msg("Retrieving repository")    
+config["YATA"]["repo"]
+proc = subprocess.run("git pull")
+if proc.returncode != 0:
+    os.chdir(oldwd)
+    tools.fatal(16, "Error " + proc.returncode + "retrieving repository")
+
+os.chdir(oldwd)
 pkgs = dict(config.items("packages"))
 for pkg in pkgs.values():
-    print (pkg)
+    #print (pkg)
     rc = make_package(pkg)   
     if (rc != 0):
-        print("ERROR MAKING PACKAGE " + pkg)
-        break
+        tools.fatal(4, "ERROR MAKING PACKAGE " + pkg)
+        
+tools.msg("Moving packages to " + os.environ("R_LIBS_SITE"))
+shutil.copytree(os.environ("R_LIBS_USER"), os.environ("R_LIBS_SITE"))
