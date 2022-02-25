a2002af79f5e501dff4e55b7c8f4107ebfd6632a
Grandez <javier.gonzalez.grandez@gmail.com>
2022-02-25 22:04:09 +0100

    Testing installation procedures

--- YATASetup/R/public.R
+++ YATASetup/R/public.R
@@ -5,7 +5,7 @@ yata_test = function() {
 yata_ports = function() {
 #    library(base)
     setup = YATASetup$new()
-    core <- system.file(package = "YATACore")
+    core <- system.file(package = "YATACore2")
     if (nchar(core) == 0) setup$fatal("YATACore is not available yet")
     core_available = suppressWarnings(require("YATACore"))
     message("Hecho")
