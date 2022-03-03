067d746f7229c93b49c566f27e0a6c1c13c896a7
Grandez <javier.gonzalez.grandez@gmail.com>
2022-03-03 16:45:11 +0100

    Redesining databases

--- YATAConfig/SQL/create_tables.sql
+++ YATAConfig/SQL/create_tables.sql
@@ -1,20 +1,20 @@
 use YATABase;
-source yata_tables_base.sql
+source tables_base.sql
 
 use YATAData;
-source yata_tables_data.sql
+source tables_data.sql
 
 use YATACI;
-source yata_tables_template.sql
+source tables_template.sql
 
 use YATA;
-source yata_tables_template.sql
+source tables_template.sql
 
 use YATATest;
-source yata_tables_template.sql
+source tables_template.sql
 
 use YATASimm;
-source yata_tables_template.sql
+source tables_template.sql
 
 use YATATemplate;
-source yata_tables_template.sql
+source tables_template.sql
