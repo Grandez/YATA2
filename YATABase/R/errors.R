cc69f16fc73d2fecc5976bc35ae4866e3ef18e9f
Grandez <javier.gonzalez.grandez@gmail.com>
2022-03-22 19:17:18 +0100

    Controlling resizing for plotly

--- YATABase/R/errors.R
+++ YATABase/R/errors.R
@@ -32,32 +32,33 @@ YATABaseCond = R6::R6Class("YATA.BASE.COND"
      }
    )
 )
+.error = function(msg, subclass=NULL, origin=NULL, ...) {
+   data = list(...)
+   data$message = msg
+   err = structure( data, class = c("YATAERROR", subclass, "error", "condition"))
+   stop(err)
+}
+
 # No se exportan para no tener efectos colaterales
-      Warning = function(msg, action=NULL, subclass=NULL, ...) {
-         warn = structure( list( message = msg,action = action, ...)
-                          ,class = c("YATAWARNING", subclass, "warning", "condition")
-          )
-          warning(warn)
-      }
-     error = function(msg, subclass=NULL, origin=NULL, ...) {
-         data = list(...)
-         data$message = msg
-         err = structure( data, class = c("YATAERROR", subclass, "error", "condition"))
-         stop(err)
-     }
-     SQL = function(msg, action=NULL, origin=NULL, ...) {
-        self$error(msg, subclass="SQL", origin=origin, action=action, ...)
-     }
-     HTTP = function(msg, action=NULL, origin=NULL, ...) {
-        self$error(msg, subclass="HTTP", origin=origin, action=action, ...)
-     }
-     EXEC = function(msg, ...) {
-        self$error(msg, subclass="EXEC", NULL, NULL, ...)
-     }
-     logical = function(msg, action=NULL, origin=NULL, ...) {
-           self$error(msg, subclass="LOGICAL", origin=origin, action=action, ...)
-     }
-     propagate = function(cond) {
-        stop(cond)
-     }
+Warning = function(msg, action=NULL, subclass=NULL, ...) {
+   warn = structure( list( message = msg,action = action, ...)
+                    ,class = c("YATAWARNING", subclass, "warning", "condition")
+    )
+    warning(warn)
+}
+ error = function(msg, subclass=NULL, origin=NULL, ...) {
+    .error(msg, subclass, origin, ...)
+}
+ SQL = function(msg, action=NULL, origin=NULL, ...) {
+     .error(msg, subclass="SQL", origin=origin, action=action, ...)
+}
+ HTTP = function(msg, action=NULL, origin=NULL, ...) {
+     .error(msg, subclass="HTTP", origin=origin, action=action, ...)
+}
+EXEC = function(msg, ...) {
+     .error(msg, subclass="EXEC", NULL, NULL, ...)
+}
+logical = function(msg, action=NULL, origin=NULL, ...) {
+     .error(msg, subclass="LOGICAL", origin=origin, action=action, ...)
+}
 
