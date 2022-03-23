52ffd5d4068d51e130cb1ffdec04b949d53d742d
Grandez <javier.gonzalez.grandez@gmail.com>
2022-03-23 11:50:02 +0100

    BBDD template changed

--- YATASetup/inst/data/SQL/init_data.sql
+++ YATASetup/inst/data/SQL/init_data.sql
@@ -19,21 +19,21 @@ INSERT INTO FIATS(ID, SYMBOL, NAME, ICON) VALUES("EUR", "EUR", "Euro"           
 INSERT INTO FIATS(ID, SYMBOL, NAME, ICON) VALUES("GBP", "GBP", "Libra Esterlina"       , "FLAG_GBP.png");
                                                                                        
 INSERT INTO FIATS(ID, SYMBOL, NAME, ICON) VALUES("INR", "INR", "Rupia India"           , "FLAG_INR.png");
-INSERT INTO FIATS(ID, SYMBOL, NAME, ICON) VALUES("ISK", "ISK", "Corona Islandesa"      , "FLAG_ISK.png")
+INSERT INTO FIATS(ID, SYMBOL, NAME, ICON) VALUES("ISK", "ISK", "Corona Islandesa"      , "FLAG_ISK.png");
 
-INSERT INTO FIATS(ID, SYMBOL, NAME, ICON) VALUES("JPK", "JPK", "Yen Japones"           , "FLAG_JPK.png")
+INSERT INTO FIATS(ID, SYMBOL, NAME, ICON) VALUES("JPK", "JPK", "Yen Japones"           , "FLAG_JPK.png");
 
-INSERT INTO FIATS(ID, SYMBOL, NAME, ICON) VALUES("MXN", "MXN", "Peso Mexicano"         , "FLAG_MXN.png")
+INSERT INTO FIATS(ID, SYMBOL, NAME, ICON) VALUES("MXN", "MXN", "Peso Mexicano"         , "FLAG_MXN.png");
 
-INSERT INTO FIATS(ID, SYMBOL, NAME, ICON) VALUES("NOK", "NOK", "Corona Noruega"        , "FLAG_NOK.png")
-INSERT INTO FIATS(ID, SYMBOL, NAME, ICON) VALUES("NZD", "NZD", "Dolar Neozelandes"     , "FLAG_NZD.png")
+INSERT INTO FIATS(ID, SYMBOL, NAME, ICON) VALUES("NOK", "NOK", "Corona Noruega"        , "FLAG_NOK.png");
+INSERT INTO FIATS(ID, SYMBOL, NAME, ICON) VALUES("NZD", "NZD", "Dolar Neozelandes"     , "FLAG_NZD.png");
 
-INSERT INTO FIATS(ID, SYMBOL, NAME, ICON) VALUES("PEN", "PEN", "Sol Peruano"           , "FLAG_PEN.png")
+INSERT INTO FIATS(ID, SYMBOL, NAME, ICON) VALUES("PEN", "PEN", "Sol Peruano"           , "FLAG_PEN.png");
 
-INSERT INTO FIATS(ID, SYMBOL, NAME, ICON) VALUES("SEK", "SEK", "Corona Sueca"          , "FLAG_SEK.png")
+INSERT INTO FIATS(ID, SYMBOL, NAME, ICON) VALUES("SEK", "SEK", "Corona Sueca"          , "FLAG_SEK.png");
 
 INSERT INTO FIATS(ID, SYMBOL, NAME, ICON) VALUES("USD", "USD", "US Dolar"              , "FLAG_USD.png");
 
-INSERT INTO FIATS(ID, SYMBOL, NAME, ICON) VALUES("XAG", "XAG", "Plata troy"            , "XAG_01.png");
-INSERT INTO FIATS(ID, SYMBOL, NAME, ICON) VALUES("XAU", "XAU", "Oro troy"              , "XAU_01.png");
+INSERT INTO FIATS(ID, SYMBOL, NAME, ICON) VALUES("XAG", "XAG", "Plata troy"            , "XAG_01.png"  );
+INSERT INTO FIATS(ID, SYMBOL, NAME, ICON) VALUES("XAU", "XAU", "Oro troy"              , "XAU_01.png"  );
           
