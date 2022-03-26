5b936258af826586e5a3b5646651d83c4c5066de
Grandez <javier.gonzalez.grandez@gmail.com>
2022-03-26 13:28:29 +0100

    removing old objects

--- YATAPUT/R/start.R
+++
@@ -1,8 +0,0 @@
-start = function(port) {
-    YATARest = YATAREST$new()
-    if (missing(port)) {
-        port = YATARest$getPort()
-    }
-    backend = BackendRserve$new()
-    backend$start(YATARest, http_port = port)
-}
