6380b2d28b487d7a1da4843569856e9296148589
Grandez <javier.gonzalez.grandez@gmail.com>
2022-03-27 12:54:19 +0200

    Daemons

--- YATABatch/R/history.R
+++ YATABatch/R/history.R
@@ -4,18 +4,18 @@ updateHistory = function(output=1, log=1) {
     batch = YATABatch$new("History", output, log)
     batch$fact$setLogger(batch$logger)
 
-    rc = tryCatch({
-       fact = YATACore::YATAFactory$new()
-       octc = fact$getObject(fact$CODES$object$currencies)
-       hist = fact$getObject(fact$CODES$object$history)
-       prov = fact$getObject(fact$CODES$object$providers)
-       ctc  = octc$getCurrencies()
-       rng  = hist$getRanges()
-       df   = left_join(ctc, rng, by=c("id", "symbol"))
-       #JGG OJO AL 2021-01-01 COMO FECHA FIJA
-       df[is.na(df$min), "min"] = as.Date.character("2021-01-01")
-       df[is.na(df$max), "max"] = as.Date.character("2021-01-01")
-       for (row in 1:nrow(ctc)) {
+    fact = YATACore::YATAFactory$new()
+    octc = fact$getObject(fact$CODES$object$currencies)
+    hist = fact$getObject(fact$CODES$object$history)
+    prov = fact$getObject(fact$CODES$object$providers)
+    ctc  = octc$getCurrencies()
+    rng  = hist$getRanges()
+    df   = left_join(ctc, rng, by=c("id", "symbol"))
+    #JGG OJO AL 2021-01-01 COMO FECHA FIJA
+    df[is.na(df$min), "min"] = as.Date.character("2021-01-01")
+    df[is.na(df$max), "max"] = as.Date.character("2021-01-01")
+    for (row in 1:nrow(ctc)) {
+         tryCatch({
            batch$logger$batch("%5d - Retrieving history for %s", row, df[row,"name"])
            data = prov$getHistory(df[row, "id"], df[row,"max"])
            if (!is.null(data)) {
@@ -26,12 +26,10 @@ updateHistory = function(output=1, log=1) {
                    Sys.sleep(1)
                }
            }
-       }
-       0
-    }, error = function(cond) {
-        message(cond)
-        16
-    })
+        }, error = function(cond) {
+            # Nada. Ignoramos errores de conexion, duplicates, etc
+        })
+   }
     batch$logger$executed(rc, begin, "Retrieving history")
-    invisible(rc)
+    invisible(0)
 }
