784595db769e72d937d3eab9e88c3f03da99c046
Grandez <javier.gonzalez.grandez@gmail.com>
2022-03-28 18:52:41 +0200

    blog module

---
+++ YATACore/R/OBJAlerts.R
@@ -0,0 +1,34 @@
+OBJAlerts = R6::R6Class("OBJ.ALERTS"
+    ,inherit    = OBJBase
+    ,portable   = FALSE
+    ,cloneable  = FALSE
+    ,lock_class = TRUE
+    ,public = list(
+        print          = function() { message("Cameras")}
+       ,initialize     = function(Factory) {
+           super$initialize(Factory)
+#           private$tblBlog   = Factory$getTable(self$codes$tables$blog)
+       }
+      #  ,add = function(data) {
+      #     data$id = Factory$getID()
+      #     data$type = DBDict$blog[[data$type]]
+      #     self$current = data
+      #     tblBlog$add(data,isolate=TRUE)
+      #     data$id
+      #  }
+      # ,select = function(idBlog) {
+      #     private$selected = FALSE
+      #     df = tblBlog$table(id=idBlog)
+      #     if (nrow(df) > 0) {
+      #         self$current = as.list(df[1,])
+      #         self$current$type = names(which(DBDict$blog == self$current$type))
+      #         private$selected = TRUE
+      #     }
+      #     invisible(self)
+      # }
+    )
+    ,private = list(
+        tblBlog = NULL
+       ,tblExchanges = NULL
+    )
+)
