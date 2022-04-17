038ed2e43ce55e112272caa24b29bc8c8054269a
Grandez <javier.gonzalez.grandez@gmail.com>
2022-04-17 13:04:42 +0200

    redesigning YATAWebShiny

--- YATAWebTemplate/www/make.bat
+++
@@ -1,7 +0,0 @@
-@SET DART=C:\SDK\sass\src\dart.exe
-@SET SASS=C:\SDK\sass\src\sass.snapshot
-
-%DART% %SASS% jgg_bootstrap.scss     jgg_bootstrap.css 
-%DART% %SASS% jgg_sass.scss          jgg_sass.css 
-%DART% %SASS% jgg_shiny.scss         jgg_shiny.css 
-%DART% %SASS% jgg_theme_default.scss jgg_theme.css 
